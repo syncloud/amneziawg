75f7ed519236e5489948e8b7d4c1e0acb210d371
Boris Rybalkin <ribalkin@gmail.com>
2026-04-23 19:16:17 +0100

    amneziawg-go: log which select case triggers shutdown
    Daemon exits in CI right after 'ip link set mtu 1420 up dev awg0' but
    same binary stays alive on build.server. Patch main.go to log which of
    term / errs / device.Wait() fires, so the next journal shows the real
    trigger instead of just 'Device closing'.

--- amneziawg-go/build.sh
+++ amneziawg-go/build.sh
@@ -20,6 +20,29 @@ wget -q \
   -O src.tar.gz
 tar xf src.tar.gz --strip-components=1 --no-same-owner --no-same-permissions
 
+# Log which select case fires in main.go so we can diagnose early shutdowns.
+python3 - <<'PY'
+import re
+p = 'main.go'
+s = open(p).read()
+old = '''	select {
+	case <-term:
+	case <-errs:
+	case <-device.Wait():
+	}'''
+new = '''	select {
+	case sig := <-term:
+		logger.Errorf("Shutting down: signal %v", sig)
+	case e := <-errs:
+		logger.Errorf("Shutting down: errs channel: %v", e)
+	case <-device.Wait():
+		logger.Errorf("Shutting down: device.Wait() fired")
+	}'''
+if old not in s:
+    raise SystemExit('select block not found')
+open(p, 'w').write(s.replace(old, new))
+PY
+
 CGO_ENABLED=0 go build \
   -trimpath \
   -ldflags "-s -w -X github.com/amnezia-vpn/amneziawg-go/ipc.socketDirectory=/var/snap/amneziawg/current/run/amneziawg" \
